20dfa08a0bb15f7fa68ddde2811f8d5e5ee3463d
thonagan-H <thonagan@yahoo.com>
2018-05-10 20:49:01 -0500

    Update index.js

--- HW 12-13/index.js
+++ HW 12-13/index.js
@@ -2,27 +2,25 @@
 var tbody = document.querySelector("tbody");
 var searchInput = document.querySelector("#searchterm");
 var searchBtn = document.querySelector("#search");
-
 searchBtn.addEventListener("click", handleSearchButtonClick);
 var filteredsearch = dataSet;
 
 function renderTable() {
     tbody.innerHTML = "";
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < filteredsearch.length; i++) {
+      
       // Get get the current  object and its fields
       var ufo_search = filteredsearch[i];
       var fields = Object.keys(ufo_search);
       // Create a new row in the tbody,
       var row = tbody.insertRow(i);
-      for (var j = 0; j < 7; j++) {
+      for (var j = 0; j < fields.length; j++) {
         // For every field in the address object, 
         // create a new cell and set its inner text to be the current value
         // at the current value's field
         var field = fields[j];
-        console.log(field)
         var cell = row.insertCell(j);
         cell.innerText = ufo_search[field];
-        console.log(j)
         }
         row.deleteCell(5)
     }
@@ -31,70 +29,33 @@ function renderTable() {
 
 function handleSearchButtonClick() {
     var filtersearch = searchInput.value.trim().toLowerCase();
-    // Set filteredAddresses to an array of all addresses whose "state" 
-    // matches the filter
-    filteredsearch = dataSet.filter(function(objects){
+    filteredsearch = datas.filter(function(objects){
         if(filtersearch == objects.city){
-          var data_city = objects.city.toLowerCase();
-          return data_city === filteredsearch;
-        }
+          var data_city = objects.city.toLowerCase();          
+          return  filteredsearch = data_city;
+        }  
         else if(filtersearch == objects.datetime){
           var data_datetime = objects.datetime.toLowerCase();
-          return data_datetime === filteredsearch;
+          return  filteredsearch = data_datetime;
         }
-        
-        // case objects.state:
-        //   var data_state = objects.state.toLowerCase();
-        //   return data_state === filteredsearch;
-        
-        // case objects.country:
-        //   var data_country = objects.country.toLowerCase();
-        //   return data_country === filteredsearch;
-        
-        // case objects.shape:
-        //   var data_shape = objects.shape.toLowerCase();
-        //   return data_shape === filteredsearch;
-
-        // case objects.comments:
-        //   var data_comments = objects.comments.toLowerCase();
-        //   return data_comments === filteredsearch;
-
-      
+         else if(filtersearch == objects.state){
+          var data_state = objects.state.toLowerCase();
+          return  filteredsearch = data_state;
+        }
+         else if(filtersearch == objects.country){
+          var data_country = objects.country.toLowerCase();
+          return  filteredsearch = data_country;
+        }
+         else if(filtersearch == objects.shape){
+          var data_shape = objects.shape.toLowerCase();
+          return  filteredsearch = data_shape;
+        }
+         else if(filtersearch == objects.comments){
+          var data_comments = objects.comments.toLowerCase();
+          return  filteredsearch = data_comments;
+        }
     });
     renderTable();
 }
 
 renderTable();
-
-// switch(objects){
-        
-//   case objects.city:
-//     filteredsearch = dataSet.filter(function(objects){
-//       var data_city = objects.city.toLowerCase();
-//       return data_city === filteredsearch;
-//     });
-//     break;
-//   case objects.datetime:
-//     filteredsearch = dataSet.filter(function(objects){
-//       var data_datetime = objects.datetime.toLowerCase();
-//       return data_datetime === filteredsearch;
-//     });
-//     break;
-//   case objects.state:
-//     filteredsearch = dataSet.filter(function(objects){
-//       var data_state = objects.state.toLowerCase();
-//       return data_state === filteredsearch;
-//     });
-//     break;
-//   case objects.country:
-//     var data_country = objects.country.toLowerCase();
-//     return data_country === filteredsearch;
-//     break;
-//   case objects.shape:
-//     var data_shape = objects.shape.toLowerCase();
-//     return data_shape === filteredsearch;
-//     break;
-//   case objects.comments:
-//     var data_comments = objects.comments.toLowerCase();
-//     return data_comments === filteredsearch;
-//     break;
