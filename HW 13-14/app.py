58e18621596e1441632c14b13b1f5f370d6bc114
thonagan-H <thonagan@yahoo.com>
2018-05-19 10:56:11 -0500

    Add files via upload

--- HW 13-14/app.py
+++ HW 13-14/app.py
@@ -1,5 +1,4 @@
 # import necessary libraries
-
 from flask import (
     Flask,
     render_template,
@@ -10,7 +9,8 @@ from flask import (
 from biodiversity import *
 
 app = Flask(__name__)
-from flask_sqlalchemy import SQLAlchemy
+
+
 
 # create route that renders index.html template
 @app.route("/")
@@ -30,19 +30,23 @@ def otu():
 def metadata(sample):
     # return jsonify(metadata_samples)
     for i in metadata_samples:
-        if i['SAMPLEID'] == int(sample.split('_')[1]):
-            return jsonify(i)
+            if i['SAMPLEID'] == int(sample.split('_')[1]):
+                        return jsonify(i)
 
 @app.route("/wfreq/<sample>")
 def wfreq(sample):
-    import df_met_csv_2
-    sampleid = sample.strip('_')[1]
+    sampleid = int(sample.split('_')[1])
     wfreq = df_met_csv_2.loc[sampleid]['WFREQ']
-    return ('The weekly washing frequency is ' + wfreq)
+    return ('The weekly washing frequency is ' + str(wfreq))                        
 
 @app.route("/samples/<sample>")
-def metadata(sample):
-    
+def samples(sample):
+    # return jsonify(metadata_samples)
+    for i in sample_vals_list:
+        for j in i.keys():
+            if j == sample:
+                return jsonify(i.get(j))
+
 
 if __name__ == "__main__":
     app.run()
